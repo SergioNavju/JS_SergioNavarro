4d46f061a665246e99432e8de06ac771d0560e46
Sergio <sdamian.navarro@gmail.com>
2021-05-13 14:16:21 -0500

    FEAT: Actividad_1js

---
+++ Act_1.js
@@ -0,0 +1,42 @@
+console.log("Hola Mundo");
+/*
+class Instructor{
+    //EN UNA CLASE LO PRIMERO QUE TENEMOS QUE PONER ES EL CONSTRUTOR
+    constructor(nombre, edad, poder=1){
+        //PONEMOS ; PORQUE NO SON PROPIEDADES NI METODOS, SON INSTRUCCIONES
+        this.nombre=nombre;
+        this.edad=edad;
+        this.poder=poder;
+    }
+
+    calificar(calif){
+        if(calif>=6){
+            console.log ('Felicidades, has aprobado.'+ this.nombre +' esta orgulloso');
+        }
+        else if (calif===5)
+        {
+            console.log ('Uy casi pasas');
+        }
+        else
+        {
+            console.log ('No pasaste , SAS');
+        }
+    }
+}
+const KIKI = new Instructor('Kiki',66);
+KIKI.maulle = function(){
+    console.log('MIAU');
+}
+KIKI.calificar =function(){
+    
+}
+console.log(KIKI);
+
+const LENIN =new Instructor("Lenin,17,2");
+LENIN.estres=0;
+LENIN.enseñar=function(){
+    console.log('Buenas, Bienvenidos al programa de entretenimiento llamado curso web');
+}
+LENIN.estresarse=function(){
+    this.estress++;
+}*/
